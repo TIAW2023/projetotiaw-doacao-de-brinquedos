57d2e526a925952f7ea4660346dd14d9d30c27a7
GabrielPen1do <128743283+GabrielPen1do@users.noreply.github.com>
2023-06-12 17:55:19 -0300

    Add files via upload

--- projetotiaw-doacao-de-brinquedos-1/codigo/home-page/user.js
+++ projetotiaw-doacao-de-brinquedos-1/codigo/home-page/user.js
@@ -1,31 +1,36 @@
-const inputFile = document.querySelector("#picture__input");
-const pictureImage = document.querySelector(".picture__image");
-const pictureImageTxt = "Choose an image";
-pictureImage.innerHTML = pictureImageTxt;
+const fotoInput = document.getElementById('picture__input');
+const nomeInput = document.getElementById('nomeInput');
+const senhaInput = document.getElementById('senhaInput');
+const fotoUsuario = document.getElementById('foto');
+const salvarButton = document.getElementById('salvarButton');
 
-inputFile.addEventListener("change", function (e) {
-  const inputTarget = e.target;
-  const file = inputTarget.files[0];
+// Preencher os campos com as informações do perfil do usuário
+fotoUsuario.src = localStorage.getItem('foto');
+nomeInput.value = localStorage.getItem('nome');
+senhaInput.value = localStorage.getItem('senha');
 
-  if (file) {
-    const reader = new FileReader();
+// Adicionar evento de clique no botão "Salvar Alterações"
+salvarButton.addEventListener('click', () => {
+    // Atualizar a foto de perfil
+    const fotoSelecionada = fotoInput.files[0];
+    if (fotoSelecionada) {
+        const fotoURL = URL.createObjectURL(fotoSelecionada);
+        fotoUsuario.src = fotoURL;
+        // Armazenar a nova foto no LocalStorage
+        localStorage.setItem('foto', fotoURL);
+    }
 
-    reader.addEventListener("load", function (e) {
-      const readerTarget = e.target;
+    // Atualizar o nome e senha do usuário
+    const novoNome = nomeInput.value;
+    const novaSenha = senhaInput.value;
+    // Faça o que for necessário para salvar as alterações
 
-      const img = document.createElement("img");
-      img.src = readerTarget.result;
-      img.classList.add("picture__img");
+    // Atualizar as informações no LocalStorage
+    localStorage.setItem('nome', novoNome);
+    localStorage.setItem('senha', novaSenha);
 
-      pictureImage.innerHTML = "";
-      pictureImage.appendChild(img);
-    });
-
-    reader.readAsDataURL(file);
-  } else {
-    pictureImage.innerHTML = pictureImageTxt;
-  }
+    // Limpar os campos de entrada
+    fotoInput.value = '';
+    nomeInput.value = '';
+    senhaInput.value = '';
 });
-
-  
- 
